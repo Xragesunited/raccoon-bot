787c57eed8dbdaab5e0b4c431a729a2131306aae
Tyrosuss <Remcovanderlinden78@gmail.com>
2020-02-10 13:46:13 +0100

    added functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client } = require("discord.js");
+const { Client, RichEmbed } = require("discord.js");
 const { config } = require("dotenv");
 
 const client = new Client({
@@ -27,7 +27,7 @@ client.on("message", async message => {
         
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
         const cmd = args.shift().toLowerCase();
-        
+        const roleColor = message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
         switch(cmd){
             case "ping" : 
                 const msg = await message.channel.send(":joy: Pinging");
@@ -35,6 +35,37 @@ client.on("message", async message => {
             break;
             case "github" :
                 message.channel.send("Github page: https://github.com/Xragesunited/raccoon-bot");
+            break;
+            case "8ball":
+                const options = ["It is certain", "Reply hazy, try again", "It is decidedly", "Ask again later", "Without a doubt", "Better not tell you now", "Yes – definitely", "Cannot predict now",
+                "You may rely on it", "Concentrate and ask again", "As I see it, yes",	"Don't count on it",	"Most likely", "My reply is no", "Outlook good", "My sources say no", "Yes", "Outlook not so good",	
+                "Signs point to yes", "Very doubtful"];
+                const question = args.join(" ");
+                const answer = options[Math.floor(Math.random() * options.length)];
+
+                const embed = new RichEmbed()
+                .setColor(roleColor)
+                .setDescription(`You asked:\n${question}\n:8ball:\nMy answer?\n${answer}`)
+                .setAuthor(client.user.username, client.user.displayAvatarURL)
+                .setTimestamp();
+                
+                message.channel.send(embed);
+            break;
+            case "say":
+                if(message.deletable) message.delete();
+                if(args.length < 1) return message.reply("i have nothing to say?").then(m => m.delete(3500));
+                
+                
+
+                if(args[0].toLowerCase() === "embed"){
+                    const embed = new RichEmbed().setColor(roleColor).setDescription(args.slice(1).join(" ")).setTimestamp()
+                    .setImage(client.user.displayAvatarURL)
+                    .setAuthor(message.author.username, message.author.displayAvatarURL)
+                    .setFooter(client.user.username, client.user.displayAvatarURL);
+
+                    message.channel.send(embed);
+                }
+
             break;
             default:
                 message.channel.send(":pensive: Could not recongnize that command");
